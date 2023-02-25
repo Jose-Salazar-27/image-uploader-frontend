fee3d22d09741f9f9710ff693f8b5945b3a417de
Jose Andres Salazar <joseandres61@gmail.com>
2023-02-25 14:44:26 -0400

    refactor: create DropZone atomic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
-import dropZoneImage from '../../assets/image.svg';
+import { DropZone } from '../atomics/DropZone';
 
 export function Form() {
+  const [files, setFiles] = useState(null);
+
+  const handleClick = e => {
+    const file = e.target.files;
+    console.log(file);
+    setFiles(file[0]);
+  };
+
   return (
     <form className={styles.container}>
       <h2 className={styles.formTitle}>Upload your image</h2>
       <p className={styles.p}>File should be jpeg, png...</p>
-      <div className={styles.dropZone}>
-        <div className={styles.imageContainer}>
-          <img src={dropZoneImage} className={styles.dropZoneImage} alt='' />
-          <p className={styles.text}>Drag an Drop your image here</p>
-        </div>
-      </div>
+      <DropZone files={files} setFiles={setFiles} />
       <p className={styles.text}>Or</p>
       <label htmlFor='button' className={styles.label}>
         Choose a file
       </label>
-      <input type='file' id='button' className={styles.button} />
+      <input type='file' id='button' className={styles.button} onChange={handleClick} />
     </form>
   );
 }
