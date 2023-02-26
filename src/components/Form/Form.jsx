75d9ccb85ac63ca0994e7b23c705bd853f531d80
Jose Andres Salazar <joseandres61@gmail.com>
2023-02-26 08:17:12 -0400

    feat: create submit function to send file

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -11,8 +11,26 @@ export function Form() {
     setFiles(file[0]);
   };
 
+  const submitFile = async e => {
+    e.preventDefault();
+
+    const payload = new FormData();
+    payload.append('my-file', files);
+
+    console.log(payload, typeof payload);
+
+    const options = {};
+
+    const result = await fetch('http://localhost:8800/api/test', {
+      method: 'POST',
+      body: payload,
+    });
+    const json = await result.json();
+    console.log(json);
+  };
+
   return (
-    <form className={styles.container}>
+    <form className={styles.container} onSubmit={e => submitFile(e)}>
       <h2 className={styles.formTitle}>Upload your image</h2>
       <p className={styles.p}>File should be jpeg, png...</p>
       <DropZone files={files} setFiles={setFiles} />
@@ -21,6 +39,7 @@ export function Form() {
         Choose a file
       </label>
       <input type='file' id='button' className={styles.button} onChange={handleClick} />
+      <button>submit</button>
     </form>
   );
 }
